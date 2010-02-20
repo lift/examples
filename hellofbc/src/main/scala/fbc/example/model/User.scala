8645e48452d063bcccb2c66594647c54932d5e6b
Jon Hoffman <jonhoffman@gmail.com>
2010-02-19 19:55:06 -0500

    added license to example files created more useful constructor for FacebookSession issue #336

--- hellofbc/src/main/scala/fbc/example/model/User.scala
+++ hellofbc/src/main/scala/fbc/example/model/User.scala
@@ -1,4 +1,21 @@
-package fbc.example.model
+/*
+ * Copyright 2010 WorldWide Conferencing, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+package fbc.example {
+package model {
 
 import _root_.net.liftweb.mapper._
 import _root_.net.liftweb.util._
@@ -10,8 +27,8 @@ import scala.xml.Text
  */
 object User extends User with MetaMegaProtoUser[User] {
   override def dbTableName = "users" // define the DB table name
-  override def screenWrap = Full(<lift:surround with="default" at="content">
-			       <lift:bind /></lift:surround>)
+  override def screenWrap = Full(<lift:surround with="default" at="content"><lift:bind /></lift:surround>)
+  
   // define the order fields will appear in forms and output
   override def fieldOrder = List(id, firstName, lastName, email,
   locale, timezone, password)
@@ -32,11 +49,11 @@ class User extends MegaProtoUser[User] {
   def validateUnique(field: MappedLong[User], msg: => String)(value:Long): List[FieldError] = value match {
     case 0 => Nil
     case _ => User.findAll(By(field,value)).filter(!_.comparePrimaryKeys(field.fieldOwner)) match {
-        case Nil => Nil
-        case x :: _ => 
-          field.set(0)
-          List(FieldError(field, Text(msg)))
-      }
+      case Nil => Nil
+      case x :: _ => 
+        field.set(0)
+        List(FieldError(field, Text(msg)))
+    }
   }
 
   object fbid extends MappedLong(this) {
@@ -44,4 +61,7 @@ class User extends MegaProtoUser[User] {
     override def dbNotNull_? = true
     override def validations = validateUnique(this, "Whoa there, this facebook account is already in use!") _ :: super.validations
   }
+}
+
+}
 }
--- hellofbc/src/main/scala/fbc/example/snippet/ConnectSnippet.scala
+++ hellofbc/src/main/scala/fbc/example/snippet/ConnectSnippet.scala
@@ -1,4 +1,21 @@
-package fbc.example.snippet
+/*
+ * Copyright 2010 WorldWide Conferencing, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+package fbc.example {
+package snippet {
 
 import fbc.example.model._
 
@@ -6,7 +23,7 @@ import _root_.scala.xml.{NodeSeq, Text}
 import _root_.net.liftweb.util._
 import _root_.net.liftweb.common._
 
-import net.liftweb.ext_api.facebook.{FacebookRestApi,FacebookClient, FacebookConnect,FacebookSession}
+import net.liftweb.ext_api.facebook.{FacebookRestApi,FacebookClient, FacebookConnect, FacebookSession}
 
 import net.liftweb.http.js.JsCmds
 import net.liftweb.http.js.JE
@@ -18,7 +35,6 @@ import JsCmds._
 import JE._
 
 import Helpers._
-
 
 class ConnectSnippet {
   
@@ -79,8 +95,8 @@ class ConnectSnippet {
     }
     redirectRoot
   }
-  
-  
-  
+}
+
+}
 }
 
