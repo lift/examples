4d5800575efc07a24a9803996acf079d426d04b8
David Pollak <feeder.of.the.bears@gmail.com>
2011-02-04 22:16:54 -0800

    Woo Hoo... Java-based LiftActors work

---
+++ liftj/src/main/java/code/comet/Yacker.java
@@ -0,0 +1,32 @@
+package code.comet;
+
+import net.liftweb.util.*;
+import net.liftweb.http.*;
+import java.util.Date;
+
+public class Yacker extends JavaCometActor {
+    public void localSetup() {
+	ping();
+	super.localSetup();
+    }
+
+    private void ping() {
+	(new ScheduleJBridge()).schedule().perform(this,
+						   new Pinger(),
+						   10000);
+    }
+
+    @Receive protected void ping(Pinger p) {
+	ping();
+	reRender();
+    }
+	
+    public RenderOut render() {
+	return nsToNsFuncToRenderOut(Css.sel("#yack", 
+					     (new Date()).toString()));
+    }
+
+    class Pinger {
+    }
+}
+
